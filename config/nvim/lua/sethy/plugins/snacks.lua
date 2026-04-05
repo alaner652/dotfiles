68fef31f82b4abe2c96c644bb79aedd49440fd75
alaner652 <hhgg12661@gmail.com>
2026-04-05 17:02:30 +0800

    indent

--- config/nvim/lua/sethy/plugins/snacks.lua
+++ config/nvim/lua/sethy/plugins/snacks.lua
@@ -143,6 +143,7 @@ return {
                         section = "terminal",
                         cmd = "ascii-image-converter ~/Pictures/mortis.png -C -c -W 50",
                         random = 15,
+                        indent = 10,
                         pane = 2,
                         height = 50,
                     },
